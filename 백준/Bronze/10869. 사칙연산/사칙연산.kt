c8899037c0962858b5723d86a27871ffb80ad406
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-01 07:25:25 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 14536 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,10 @@
+fun main(){
+  val (num1, num2) = readln().split(" ")
+  val n1 = num1.toInt()
+  val n2 = num2.toInt()
+  println(n1+n2)
+  println(n1-n2)
+  println(n1*n2)
+  println(n1/n2)
+  println(n1%n2)
+}
